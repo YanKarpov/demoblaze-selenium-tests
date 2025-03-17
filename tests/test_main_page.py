ca4b73b3b202bbdb6deeb4457d16f3dc91cb9070
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-03-17 22:01:51 +0300

    fix import config

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,6 @@
 import pytest
 from pages.main_page import MainPage
-from config.config import BASE_URL
+from config.config import MAIN_URL
 
 @pytest.mark.smoke
 class TestMainPageLoad:
@@ -8,19 +8,19 @@ class TestMainPageLoad:
 
     def test_home_page_loads(self, browser):
         """Проверяет, что главная страница загружается"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert page.is_banner_visible(), "Главный баннер не отображается"
 
     def test_page_title(self, browser):
         """Проверяет, что заголовок страницы корректный"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert browser.title == "STORE", "Некорректный заголовок страницы"
 
     def test_navigation_bar_present(self, browser):
         """Проверяет наличие навигационной панели"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert page.is_nav_bar_visible(), "Навигационная панель отсутствует"
 
@@ -30,13 +30,13 @@ class TestMainPageButtons:
 
     def test_login_button_is_clickable(self, browser):
         """Проверяет, что кнопка 'Login' кликабельна"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert page.is_login_button_clickable(), "Кнопка логина не кликабельна"
 
     def test_signup_button_is_clickable(self, browser):
         """Проверяет, что кнопка 'Sign up' кликабельна"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert page.is_signup_button_clickable(), "Кнопка регистрации не кликабельна"
 
@@ -46,6 +46,6 @@ class TestMainPageContent:
 
     def test_products_are_displayed(self, browser):
         """Проверяет, что на странице есть товары"""
-        page = MainPage(browser, BASE_URL)
+        page = MainPage(browser, MAIN_URL)
         page.open()
         assert page.is_products_displayed(), "Товары не отображаются"
