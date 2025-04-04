7d3113db98fcaf5fb451cffdc2d141ec9967c2e0
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-04-04 17:54:06 +0300

    import logger in base page

--- utils/logger.py
+++ utils/logger.py
@@ -1,40 +1,25 @@
 import logging
 import os
-import glob
+from logging.handlers import RotatingFileHandler
 
 log_dir = "logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+os.makedirs(log_dir, exist_ok=True)  
 
+log_file = os.path.join(log_dir, "meow_logger.log")
 max_size = 5 * 1024 * 1024  
-
-def get_next_log_file():
-    existing_logs = sorted(glob.glob(os.path.join(log_dir, "meow_logger_*.log")))
-    if existing_logs:
-        last_log = existing_logs[-1]
-        last_index = int(last_log.split("_")[-1].split(".")[0])
-        new_index = last_index + 1
-    else:
-        new_index = 1
-    return os.path.join(log_dir, f"meow_logger_{new_index}.log")
-
-log_file = get_next_log_file()
+backup_count = 5  
 
 logger = logging.getLogger("meow_logger")
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler(log_file, encoding="utf-8")
+file_handler = RotatingFileHandler(log_file, maxBytes=max_size, backupCount=backup_count, encoding="utf-8")
 file_handler.setLevel(logging.INFO)
 
-# Форматирование логов
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 if not logger.handlers:
     logger.addHandler(file_handler)
-
-# Проверяем, если текущий файл уже полный, создаем новый
-current_size = os.path.getsize(log_file) if os.path.exists(log_file) else 0
 
 # тестировал рабобту ограничения лог файла
 # while current_size < max_size:
